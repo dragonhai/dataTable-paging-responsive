ad8a0046920c40c54d28cbc46f8ec09862209eb4
Bui Thanh Hai <b.thanhhai@gmail.com>
2022-06-05 08:52:09 +0700

    Update bootstrap 4/5 page

--- docs/assets/js/dashboard.js
+++ docs/assets/js/dashboard.js
@@ -1,21 +1,29 @@
 (function ($) {
+    'use strict';
+
+    let prefixUrl = 'assets/';
+    let enGBUrl = prefixUrl + 'i18n/en-GB.json';
+    let viUrl = prefixUrl + 'i18n/vi.json';
+    let emptyDataUrl = prefixUrl + 'ajax/empty.json';
+    let result1Url = prefixUrl + 'ajax/result1.json';
+    let result2Url = prefixUrl + 'ajax/result2.json';
     $('#datatable-example1').DataTable({
-        language: { url: 'i18n/en-GB.json' },
-        ajax: 'ajax/empty.json',
+        language: { url: enGBUrl },
+        ajax: emptyDataUrl,
         scrollX: true,
         scrollCollapse: true,
     });
 
     $('#datatable-example2').DataTable({
-        language: { url: 'i18n/en-GB.json' },
-        ajax: 'ajax/result1.json',
+        language: { url: enGBUrl },
+        ajax: result1Url,
         scrollX: true,
         scrollCollapse: true,
     });
 
     $('#datatable-example3').DataTable({
-        language: { url: 'i18n/vi.json' },
-        ajax: 'ajax/result2.json',
+        language: { url: viUrl },
+        ajax: result2Url,
         scrollX: true,
         scrollCollapse: true,
     });
